d223e9817bac1947df6368e4fb4ce0f0c14322cd
zzinue <zzinue@gmail.com>
2023-01-25 17:04:36 -0600

    adding surprise me button

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -29,6 +29,15 @@ const FormField = ({
           </button>
         )}
       </div>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-[#4649ff]
+        focus:border-[4649ff] outline-none block w-full p-3"
+      />
     </div>
   );
 };
